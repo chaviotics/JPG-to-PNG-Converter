83022c5c20993410cae7bfdc16d7afcad8fa4787
Chav Sumalinog <chaviotics@gmail.com>
2021-02-04 13:51:46 +0800

    Added more modules  and instructions.

--- jpgToPng.py
+++ jpgToPng.py
@@ -1,1 +1,15 @@
 from PIL import Image, ImageFilter
+import sys
+import os 
+
+# input:
+# jpgToPng.py \Pictures \new
+
+# grab first and second argument -> Use sys.argv
+# check if \new folder exists, if not, create the folder
+
+# loop through the pictures file
+# convert images to png
+# save to the new folder
+
+
